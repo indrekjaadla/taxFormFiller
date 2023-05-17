afd8e33d453f481c6a2df71d54b96f4826667bf5
indrekjaadla <indrekjaadla@gmail.com>
2023-05-17 09:54:09 +0300

    Constructor introduced for class Entry.

--- src/main/java/Entry.java
+++ src/main/java/Entry.java
@@ -1,7 +1,6 @@
 public class Entry {
     public String id;
     public String description;
-    public String country = "Ameerika";
     public String closingDate;
     public String closingQuantity;
     public String openingDate;
@@ -9,5 +8,20 @@ public class Entry {
     public String sellingFee;
     public String sellingCost;
     public String withholdingTax = "0";
+    public String country = "Ameerika";
     public String currency = "USD";
+
+    public Entry(String[] row) {
+        this.id = row[0];
+        this.description = row[1];
+        this.closingQuantity = row[2];
+        this.openingDate = row[3];
+        this.buyingCost = row[4];
+        this.sellingFee = row[5];
+        this.sellingCost = row[6];
+        this.withholdingTax = row[7];
+        this.country = row[8];
+        this.currency = row[9];
+        this.closingDate = row[10];
+    }
 }
