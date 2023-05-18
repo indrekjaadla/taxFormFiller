c149f4e463d73affc0882ce7122fe013374862d1
indrekjaadla <indrekjaadla@gmail.com>
2023-05-18 23:24:47 +0300

    custom constructor removed

--- src/main/java/Entry.java
+++ src/main/java/Entry.java
@@ -2,26 +2,12 @@ public class Entry {
     public String id;
     public String description;
     public String closingDate;
-    public String closingQuantity;
+    public int closingQuantity;
     public String openingDate;
-    public String buyingCost;
-    public String sellingFee;
-    public String sellingCost;
-    public String withholdingTax;
+    public double buyingCost;
+    public double sellingFee;
+    public double sellingCost;
+    public double withholdingTax;
     public String country;
     public String currency;
-
-    public Entry(String[] row) {
-        this.id = row[0];
-        this.description = row[1];
-        this.closingQuantity = row[2];
-        this.openingDate = row[3];
-        this.buyingCost = row[4];
-        this.sellingFee = row[5];
-        this.sellingCost = row[6];
-        this.withholdingTax = row[7];
-        this.country = row[8];
-        this.currency = row[9];
-        this.closingDate = row[10];
-    }
 }
