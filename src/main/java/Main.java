6df0cf0ebb950d2c75d4260997f9bc85c12753ad
indrekjaadla <indrekjaadla@gmail.com>
2023-05-20 19:54:49 +0300

    pause() changed and pauses moved from main() to sub-methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,10 @@ import java.util.List;
 public class Main {
 
     public static WebDriver webDriver;
+    public static final int waitAfterAdd = 1000;
+    public static final int waitAfterClick = 200;
+    public static final int waitAfterSend = 500;
+    public static final int waitBeforeSave = 500;
 
     public static void main(String[] args) {
 
@@ -21,47 +25,23 @@ public class Main {
         webDriver = new ChromeDriver(chromeOptions);
 
         List<Entry> entries = Entry.getEntriesFromExcelSheet(Excel.pathName2021);
-
         addTransactionRow();
-        pause_500();
         for (Entry entry : entries) {
-
             insertId(entry);
-            pause_500();
-
             insertDescription(entry);
-            pause_500();
-
             insertCountry(entry);
-            pause_500();
-
             insertAssetCategory();
-            pause_500();
-
             insertClosingDate(entry);
-            pause_500();
-
             insertClosingQuantity(entry);
-            pause_500();
-
             insertBuyingCost(entry);
-            pause_500();
-
             insertSellingFee(entry);
-            pause_500();
-
             insertSellingCost(entry);
-            pause_500();
-
             insertPaidIncomeTax(entry);
-            pause_500();
-
             saveTransactionRow();
-            pause_500();
+            pause(1000);
         }
-
         cancelTransactionRow();
-        pause_500();
+        pause(1000);
         webDriver.quit();
     }
 
@@ -84,9 +64,9 @@ public class Main {
         );
     }
 
-    private static void pause_500() {
+    private static void pause(int timeInMillis) {
         try {
-            Thread.sleep(500);
+            Thread.sleep(timeInMillis);
         } catch (InterruptedException e) {
             System.err.format("IOException: %s%n", e);
         }
@@ -94,69 +74,92 @@ public class Main {
 
     private static void addTransactionRow() {
         webDriver.findElement(By.xpath("//*[@id=\"stockfunds-new-row-button\"]")).click();
+        pause(waitAfterAdd);
     }
 
     private static void insertId(Entry entry) {
         webDriver.findElement(By.xpath("//*[@id=\"add_stockfunds_isinCode\"]")).sendKeys(entry.id);
+        pause(waitAfterSend);
     }
 
     private static void insertDescription(Entry entry) {
         webDriver.findElement(By.xpath("//*[@id=\"add_stockfunds_name\"]")).sendKeys(entry.description);
+        pause(waitAfterSend);
     }
 
     private static void insertCountry(Entry entry) {
         webDriver.findElement(By.xpath("//*[@id=\"add_stockfunds_state\"]")).click();
+        pause(waitAfterClick);
         webDriver.findElement(By.xpath("//*[@id=\"select-user-input-element\"]")).sendKeys(entry.country);
+        pause(waitAfterSend);
         webDriver.findElement(By.xpath("//*[@id=\"add_stockfunds_state-1\"]/span")).click();
+        pause(waitAfterClick);
     }
 
     private static void insertAssetCategory() {
         webDriver.findElement(By.xpath("//*[@id=\"add_stockfunds_type\"]")).click();
+        pause(waitAfterClick);
         webDriver.findElement(By.xpath("//*[@id=\"add_stockfunds_type-0\"]/span")).click();
+        pause(waitAfterClick);
     }
 
     private static void insertClosingDate(Entry entry) {
         webDriver.findElement(By.xpath("//*[@id=\"add_stockfunds_date\"]")).sendKeys(entry.closingDate);
+        pause(waitAfterSend);
     }
 
     private static void insertClosingQuantity(Entry entry) {
         webDriver.findElement(By.xpath("//*[@id=\"add_stockfunds_amount\"]")).sendKeys(entry.closingQuantity);
+        pause(waitAfterSend);
     }
 
     private static void insertBuyingCost(Entry entry) {
         webDriver.findElement(By.xpath("//*[@id=\"add-stockfunds-cost-amount-calculator-link\"]")).click();
+        pause(waitAfterClick);
         fillAndCloseCurrencyCalculator(entry.buyingCost, entry.openingDate, entry.currency);
     }
 
     private static void insertSellingCost(Entry entry) {
         webDriver.findElement(By.xpath("//*[@id=\"add-stockfunds-selling-price-calculator-link\"]")).click();
+        pause(waitAfterClick);
         fillAndCloseCurrencyCalculator(entry.sellingCost, entry.closingDate, entry.currency);
     }
 
     private static void insertSellingFee(Entry entry) {
         webDriver.findElement(By.xpath("//*[@id=\"add-stockfunds-appropriation-cost-calculator-link\"]")).click();
+        pause(waitAfterClick);
         fillAndCloseCurrencyCalculator(entry.sellingFee, entry.closingDate, entry.currency);
     }
 
     private static void insertPaidIncomeTax(Entry entry) {
         webDriver.findElement(By.xpath("//*[@id=\"add-stockfunds-income-tax-calculator-link\"]")).click();
+        pause(waitAfterClick);
         fillAndCloseCurrencyCalculator(entry.incomeTax, entry.closingDate, entry.currency);
     }
 
     private static void fillAndCloseCurrencyCalculator(String amount, String date, String currency) {
         webDriver.findElement(By.xpath("//*[@id=\"currencySum\"]")).sendKeys(amount);
+        pause(waitAfterSend);
         webDriver.findElement(By.xpath("//*[@id=\"currency\"]")).click();
+        pause(waitAfterClick);
         webDriver.findElement(By.xpath("//*[@id=\"select-user-input-element\"]")).sendKeys(currency);
+        pause(waitAfterSend);
         webDriver.findElement(By.xpath("//*[@id=\"currency-0\"]/span")).click();
+        pause(waitAfterClick);
         webDriver.findElement(By.xpath("//*[@id=\"currencyDate\"]")).sendKeys(date);
+        pause(waitAfterSend);
         webDriver.findElement(By.xpath("//*[@id=\"currency-calculator-calculate-button\"]")).click();
+        pause(waitAfterClick);
     }
 
     private static void saveTransactionRow() {
+        pause(waitBeforeSave);
         webDriver.findElement(By.xpath("//*[@id=\"add-stockfunds-save-button\"]")).click();
+        pause(waitAfterClick);
     }
 
     private static void cancelTransactionRow() {
         webDriver.findElement(By.xpath("//*[@id=\"add-stockfunds-cancel-button\"]")).click();
+        pause(waitAfterClick);
     }
 }
